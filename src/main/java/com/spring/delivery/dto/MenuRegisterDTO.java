3fa254cb683a834915a9b1419e4ea08b02736dc2
SSH4240 <pdxucxy@gmail.com>
2023-06-11 21:28:10 +0900

    test

---
+++ src/main/java/com/spring/delivery/dto/MenuRegisterDTO.java
@@ -0,0 +1,16 @@
+package com.spring.delivery.dto;
+
+import com.spring.delivery.domain.MenuType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MenuRegisterDTO {
+    private String name;
+    private MenuType menuType;
+    private int price;
+    private String description;
+    private String imageName;
+    private Long storeId;
+}
---
+++ src/main/java/com/spring/delivery/dto/MenuUpdateDTO.java
@@ -0,0 +1,11 @@
+package com.spring.delivery.dto;
+
+import lombok.Data;
+
+@Data
+public class MenuUpdateDTO {
+    private String name;
+    private String updatedName;
+    private int price;
+    private String description;
+}
