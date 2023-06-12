3678a34c46c2c0d80417bb9da4beb477d5163b1a
SSH4240 <pdxucxy@gmail.com>
2023-06-12 12:02:40 +0900

    test

--- src/main/java/com/spring/delivery/controller/UserController.java
+++ src/main/java/com/spring/delivery/controller/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/customer")
+@RequestMapping("/api/info")
 @RequiredArgsConstructor
 public class UserController {
 
